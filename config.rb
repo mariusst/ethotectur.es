2c98aae56e2e555fc40eb239a3805357052cc8cc
Marius Stoica <mariustoica@gmail.com>
2016-11-05 18:23:51 +0200

    using only slim template language, no haml; fixes for doar maine 2 project

--- config.rb
+++ config.rb
@@ -37,8 +37,16 @@ end
 # Build-specific configuration
 configure :build do
   # Minify CSS on build
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 end
+
+require 'slim'
+
+# Set slim-lang output style
+Slim::Engine.set_options :pretty => true
+
+# Set template languages
+set :slim, :layout_engine => :slim
