fb9c2d79c043250d0214c97bab7006c81e73b24f
Marius Stoica <mariustoica@gmail.com>
2017-06-24 21:09:47 +0300

    new project updated

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -3,6 +3,7 @@ var bLazy = new Blazy({
         // Options
     });
 
+/*
 document.addEventListener('play', function(e){
     var videos = document.getElementsByTagName('video');
     for(var i = 0, len = videos.length; i < len;i++){
@@ -11,3 +12,9 @@ document.addEventListener('play', function(e){
         }
     }
 }, true);
+*/
+
+// Pause all other HTML5 videos when a video is played
+$('video').click(function() {
+    $("video").each(function () { this.pause() });
+});
