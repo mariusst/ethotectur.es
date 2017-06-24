cc48d7c3c29a38d65cb95b390c2a00fb285ef991
Marius Stoica <mariustoica@gmail.com>
2017-06-24 21:02:13 +0300

    new project updated

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -2,3 +2,12 @@ $(document).foundation();
 var bLazy = new Blazy({
         // Options
     });
+
+document.addEventListener('play', function(e){
+    var videos = document.getElementsByTagName('video');
+    for(var i = 0, len = videos.length; i < len;i++){
+        if(videos[i] != e.target){
+            videos[i].pause();
+        }
+    }
+}, true);
